7171edab1d22000c10dfc30870d4eb8f01ad3e82
pockyee <baoqi85@gmail.com>
2025-09-24 14:29:40 +0000

    update responsive design

--- data.js
+++ data.js
@@ -13,11 +13,33 @@ export const menuArray = [
         emoji: "🍔",
         id: 1
     },
+
+    {
+        name: "Hot Dog",
+        ingredients: ["sausage", "pickles", "onion"],
+        id: 2,
+        price: 10,
+        emoji: "🌭"
+    },
+    {
+        name: "Taco",
+        ingredients: ["beef", "cheese", "chili"],
+        price: 7,
+        emoji: "🌮",
+        id: 3
+    },
         {
         name: "Beer",
         ingredients: ["grain, hops, yeast, water"],
         price: 12,
         emoji: "🍺",
-        id: 2
+        id: 4
+    },
+        {
+        name: "Bubble Tea",
+        ingredients: ["milk, tea, tapioca balls"],
+        price: 10,
+        emoji: "🧋",
+        id: 5
     }
 ]
--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const itemList = document.getElementById('item-list')
 function buildItem(obj) {
     return `
         <container class="item item${obj.id}">
-            <h6>${obj.emoji}</h6>
+            <h3>${obj.emoji}</h6>
             <div class="description">
-                <h3>${obj.name}</h3>
-                <h4>${obj.ingredients.join(', ')}</h4>
-                <h5>${obj.price}</h5>                   
+                <h4>${obj.name}</h3>
+                <h5>${obj.ingredients.join(', ')}</h4>
+                <h6>$ ${obj.price}</h5>                   
             </div>
             <button class="plus">+</button>
         </container>
